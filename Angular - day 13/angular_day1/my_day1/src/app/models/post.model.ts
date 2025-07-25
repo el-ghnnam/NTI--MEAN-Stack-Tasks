238faf04c32dc2f75220c3d9c9d177ce52ef9fce
Ahmed Ibrahim <ahmedibrahimghnnam012@gmail.com>
2025-07-25 22:57:45 +0300

    Update Angular - day12 Lab to Angular - Labs

--- Angular - day 13/angular_day1/my_day1/src/app/models/post.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Ipost {
-  title: string;
-  body: string;
-  id: number;
-  userid: number;
-  date: Date;
-  imgurl: String;
-  like: boolean;
-}
--- Angular - day 13/angular_day1/my_day1/src/app/models/user.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Iuser{
-  id:number,
-  name:string,
-  imgurl:string,
-  password:string,
-}
